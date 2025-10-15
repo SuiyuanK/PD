4929f708b7eba12c84e9a1f1491f886bf21eb559
Kong <3334108165@qq.com>
2025-10-15 22:37:47 +0800

    merge

---
+++ 04_pv/merge/Calibredrv/merge.tcl
@@ -0,0 +1,15 @@
+layout filemerge -mode append -precision 1000.0 -verbose -topcell top \
+    -in /mnt/hgfs/Em/PD/04_pv/data/pr_outputs/top.gds.gz \
+    -in /mnt/hgfs/Em/PD/04_pv/data/layout_files/SP40NLLD2RN_3P3V_V0p5_7MT_1TM.gds \
+    -in /mnt/hgfs/Em/PD/04_pv/data/layout_files/scc40nll_vhsc40_hvt.gds \
+    -in /mnt/hgfs/Em/PD/04_pv/data/layout_files/scc40nll_vhsc40_lvt.gds \
+    -in /mnt/hgfs/Em/PD/04_pv/data/layout_files/scc40nll_vhsc40_rvt.gds \
+    -in /mnt/hgfs/Em/PD/04_pv/data/layout_files/RAMSP1024X16.gds2 \
+    -in /mnt/hgfs/Em/PD/04_pv/data/layout_files/RAMSP128X16.gds2 \
+    -in /mnt/hgfs/Em/PD/04_pv/data/layout_files/RAMSP2048X16.gds2 \
+    -in /mnt/hgfs/Em/PD/04_pv/data/layout_files/RAMSP4096X16.gds2 \
+    -in /mnt/hgfs/Em/PD/04_pv/data/layout_files/RAMTP1024X16.gds2 \
+    -in /mnt/hgfs/Em/PD/04_pv/data/layout_files/RAMTP128X16.gds2 \
+-out /mnt/hgfs/Em/PD/04_pv/merge/Calibredrv/top_merge.gds
+
+
