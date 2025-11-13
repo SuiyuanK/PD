2b1820384aff67e573d007debef912711752f512
Kong <3334108165@qq.com>
2025-11-13 14:31:35 +0800

    run starrc脚本

---
+++ 03_pt/starrc/run_starrc.sh
@@ -0,0 +1,22 @@
+cd ./cmax_125c
+./cmax_125c.run &
+cd ../cmax_m40c
+./cmax_m40c.run &
+
+cd ../cmin_125c
+./cmin_125c.run &
+cd ../cmin_m40c
+./cmin_m40c.run &
+
+cd ../rcmax_125c
+./rcmax_125c.run &
+cd ../rcmax_m40c
+./rcmax_m40c.run &
+
+cd ../rcmin_125c
+./rcmin_125c.run &
+cd ../rcmin_m40c
+./rcmin_m40c.run 
+
+exit
+
