8c5c5fdc2b39ae040923ba826f4d1ae83858f427
Kong <3334108165@qq.com>
2025-11-14 18:34:10 +0800

    调整

--- 05_power/ele_static_power/write_timing_file.tcl
+++
@@ -1,6 +0,0 @@
-source /export/SoftWare/Ansys/Redhawk2021/RedHawk_Linux64e6_V2021R1.1/bin/pt2timing.tcl
-
-set ADS_ALLOWED_PCT_OF_NON_CLOCKED_REGISTERS 30
-
-getSTA * -b -output data/image_process
-
