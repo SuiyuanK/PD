d81bbf359ac6b0902333d07b34405ad4dc581f42
Kong <3334108165@qq.com>
2025-12-01 20:13:04 +0800

    04_pv更新

---
+++ 04_pv/lvs/gen_spi.sh
@@ -0,0 +1,14 @@
+# awk '{ gsub(/^PLCORNER/,"\/\/PLCORNER"); print $0 }' aes_ASIC_sim.vg > aes_ASIC.vg
+
+
+# 有只读cdl的 
+
+# -s0 GND -s1 VDD              -l ./verilog/RAMSP1024X16.v -l ./verilog/RAMTP1024X16.v -l ./verilog/scc40nll_vhsc40_hvt_pg.v -l ./verilog/scc40nll_vhsc40_lvt_pg.v -l ./verilog/scc40nll_vhsc40_rvt_pg.v \
+v2lvs -64 -v /mnt/hgfs/Em/PD/eco/outputs/image_icb.pg.v.gz \
+      -s ./cdl/RAMSP1024X16.cdl -s ./cdl/RAMTP1024X16.cdl -s ./cdl/scc40nll_vhsc40_hvt.cdl -s ./cdl/scc40nll_vhsc40_lvt.cdl -s ./cdl/scc40nll_vhsc40_rvt.cdl\
+      -s ./cdl/empty_subckt.sp \
+      -o top.spi 
+
+
+# echo '.GLOBAL VDD' >> top.spi
+# echo '.GLOBAL VSS' >> top.spi
---
+++ 04_pv/merge/Calibredrv/merge_dummy.tcl
@@ -0,0 +1,6 @@
+layout filemerge -precision 1000.0 -verbose -createtop TOP \
+    -in ./top_merge.gds \
+    -in ./Dummy.gds \
+-out ./top_merge_dummy.gds
+
+
